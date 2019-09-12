6ab0c867e817bc5a9028f7f4afac17f5ed075b1a
Dmitrii Smolkin <smolkin.do@gmail.com>
2019-09-12 16:31:58 +0300

    Booking remplate mobile version width fix

--- resources/views/public/booking.blade.php
+++ resources/views/public/booking.blade.php
@@ -10,8 +10,8 @@
 
 @section("content")
     <main>
-        <div class="container py-4 d-flex justify-content-center">
-            <div class="w-50">
+        <div class="container py-4 d-flex justify-content-center mx-auto">
+            <div class="col-12 col-sm-12 col-md-8">
                 <iframe src="https://samo.energotour.com/fast_search" frameborder="0" height="387px" width="100%" scrolling="no"></iframe>
             </div>
         </div>
