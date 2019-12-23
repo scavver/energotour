4ac62f856bc16f236fc0ec2b66dd23ea41c1edbc
Dmitrii Smolkin <smolkin.do@gmail.com>
2019-12-23 09:24:45 +0300

    404 Custom view

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,20 @@
+@extends("layouts.avia")
+
+@section("title")
+    Страница не найдена
+@endsection
+
+@section("description")
+    Возможно страница была перемещена или удалена.
+@endsection
+
+@section("content")
+    <div class="container bg-white">
+        <h3 class="pt-3 pb-3 mb-3 text-center bordered-bottom">Страница не найдена</h3>
+        <div style="height: 50vh; text-align: center">
+            <h4>Сайт компании Энерго – Тур был обновлен.</h4>
+            <p>Возможно данная страница была перемещена по другому адресу, пожалуйста воспользуйтесь навигацией сайта.</p>
+        </div>
+
+    </div>
+@endsection
